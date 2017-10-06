fb836ea345afc13296528630fb0ebaf4ee53ea30
dowskee <31831031+dowskee@users.noreply.github.com>
2017-10-06 14:32:52 -0400

    This is the first commit

---
+++ Lab9/Circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Circle
+    {
+        
+        public double radius;
+        public double area;
+
+        public double Radius
+        {
+            set { radius = value; }
+
+            get { return radius; }
+
+        }
+
+        public double Area
+        {
+            set { area = value; }
+            get { return area; }
+        }
+
+
+        public double GetArea()
+        {
+            Console.WriteLine();
+        }
+
+        public double GetCurcumference()
+        {
+
+        }
+    }
+}
